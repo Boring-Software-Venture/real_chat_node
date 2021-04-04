9079907f705edd3df5aa7d7918246d5b5dfc28a9
manavmodi22 <manavmodi0004@gmail.com>
2021-04-04 19:50:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http);
+
 app.get('/', (req, res) => {
-    res.send("Node server is running. YAAY!")
+    res.send("Node Server is running. Yay!!")
 })
 
-// http.listen(8080)
-
 io.on('connection', socket => {
+    //Get the chatID of the user and join in a room of the same name
     chatID = socket.handshake.query.chatID
     socket.join(chatID)
+
+    //Leave the room if the user closes the socket
     socket.on('disconnect', () => {
         socket.leave(chatID)
     })
+
+    //Send message to only a particular user
     socket.on('send_message', message => {
         receiverChatID = message.receiverChatID
         senderChatID = message.senderChatID
         content = message.content
 
+        //Send message to only that particular id
         socket.in(receiverChatID).emit('receive_message', {
             'content': content,
             'senderChatID': senderChatID,
-            'receiverChatID': receiverChatID
+            'receiverChatID':receiverChatID,
         })
     })
 });
-
-const socketio = require('socket.io')(http)
-
-socketio.on("connection", (userSocket) => {
-    userSocket.on("send_message", (data) => {
-        userSocket.broadcast.emit("receive_message",data)
-    })
-})
 
 http.listen(process.env.PORT)
