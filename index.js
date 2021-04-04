800947b6c3c0c0f1509901ebd4a0dfbc3b08756a
manavmodi22 <manavmodi0004@gmail.com>
2021-04-04 14:56:45 +0530

    one-to-one feature

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const app = require('express')()
 const http = require('http').createServer(app)
+const io = require('socket.io')(http);
 app.get('/', (req, res) => {
     res.send("Node server is running. YAAY!")
 })
 
 // http.listen(8080)
+
+io.on('connection', socket => {
+    chatID = socket.handshake.query.chatID
+    socket.join(chatID)
+    socket.on('disconnect', () => {
+        socket.leave(chatID)
+    })
+    socket.on('send_message', message => {
+        receiverChatID = message.receiverChatID
+        senderChatID = message.senderChatID
+        content = message.content
+
+        socket.in(receiverChatID).emit('receive_message', {
+            'content': content,
+            'senderChatID': senderChatID,
+            'receiverChatID': receiverChatID
+        })
+    })
+});
 
 const socketio = require('socket.io')(http)
 
